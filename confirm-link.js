9da9bc62ba28b4d3fc305790155e18491a1011cd
Prabhsimran Singh (Simar Harneja) <psharneja@gmail.com>
2020-05-19 19:56:56 +0530

    extending

---
+++ confirm-link.js
@@ -0,0 +1,13 @@
+class ConfirmLink extends HTMLAnchorElement {
+    connectedCallback() {
+        this.addEventListener('click', event =>{
+            if(!confirm('do you really want to leave')) {
+                event.preventDefault();
+            }
+        })
+    }
+
+}  
+
+
+customElements.define('my-confirm-link', ConfirmLink, { extends: 'a'});
